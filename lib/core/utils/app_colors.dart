0a668e92e109a3177a1005bbd418aa6778a75602
DELL <oo9264667@gmail.com>
2024-01-07 06:06:29 -0800

    Assets Added ✔️

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color primary = Color(0xD9D0C1);
+  static const Color brown = Color(0x3E2618);
+  static const Color blue = Color(0x1D2939);
+}

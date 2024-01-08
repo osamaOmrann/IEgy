e392cfb9069f7176b672c0b9cdad9d28cd1bb3e0
DELL <oo9264667@gmail.com>
2024-01-08 13:48:21 -0800

    Splash ⏳

--- lib/features/splash/presentation/screens/welcome_screen.dart
+++ lib/features/splash/presentation/screens/welcome_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:iegy/core/utils/app_assets.dart';
+import 'package:iegy/core/utils/app_colors.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class WelcomeScreen extends StatefulWidget {
@@ -14,146 +16,77 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    bool first = true;
     final pages = List.generate(
         2,
-        (index) =>
-            Image.asset(index == 0 ? AppAssets.splash1 : AppAssets.splash2));
+        (index) => Image.asset(
+              index == 0 ? AppAssets.splash1 : AppAssets.splash2,
+            ));
 
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(height: 16),
-              SizedBox(
-                height: 240,
-                child: PageView.builder(
-                  controller: controller,
-                  // itemCount: pages.length,
-                  itemBuilder: (_, index) {
-                    return pages[index % pages.length];
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 24, bottom: 12),
-                child: Text(
-                  'Worm',
-                  style: TextStyle(color: Colors.black54),
-                ),
-              ),
-              SmoothPageIndicator(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 420.h,
+              width: 430.w,
+              child: PageView.builder(
                 controller: controller,
-                count: pages.length,
-                effect: const WormEffect(
-                  dotHeight: 16,
-                  dotWidth: 16,
-                  type: WormType.thinUnderground,
-                ),
+                itemCount: pages.length,
+                itemBuilder: (_, index) {
+                  return pages[index % pages.length];
+                },
+                onPageChanged: (_) => setState(() => first = !first),
               ),
-              //
-              // Padding(
-              //   padding: const EdgeInsets.only(top: 16, bottom: 8),
-              //   child: Text(
-              //     'Jumping Dot',
-              //     style: TextStyle(color: Colors.black54),
-              //   ),
-              // ),
-              // Container(
-              //   child: SmoothPageIndicator(
-              //     controller: controller,
-              //     count: pages.length,
-              //     effect: JumpingDotEffect(
-              //       dotHeight: 16,
-              //       dotWidth: 16,
-              //       jumpScale: .7,
-              //       verticalOffset: 15,
-              //     ),
-              //   ),
-              // ),
-              // Padding(
-              //   padding: const EdgeInsets.only(top: 16, bottom: 12),
-              //   child: Text(
-              //     'Scrolling Dots',
-              //     style: TextStyle(color: Colors.black54),
-              //   ),
-              // ),
-              // SmoothPageIndicator(
-              //     controller: controller,
-              //     count: pages.length,
-              //     effect: ScrollingDotsEffect(
-              //       activeStrokeWidth: 2.6,
-              //       activeDotScale: 1.3,
-              //       maxVisibleDots: 5,
-              //       radius: 8,
-              //       spacing: 10,
-              //       dotHeight: 12,
-              //       dotWidth: 12,
-              //     )),
-              // Padding(
-              //   padding: const EdgeInsets.only(top: 16, bottom: 16),
-              //   child: Text(
-              //     'Customizable Effect',
-              //     style: TextStyle(color: Colors.black54),
-              //   ),
-              // ),
-              // Container(
-              //   // color: Colors.red.withOpacity(.4),
-              //   child: SmoothPageIndicator(
-              //     controller: controller,
-              //     count: pages.length,
-              //     effect: CustomizableEffect(
-              //       activeDotDecoration: DotDecoration(
-              //         width: 32,
-              //         height: 12,
-              //         color: Colors.indigo,
-              //         rotationAngle: 180,
-              //         verticalOffset: -10,
-              //         borderRadius: BorderRadius.circular(24),
-              //         // dotBorder: DotBorder(
-              //         //   padding: 2,
-              //         //   width: 2,
-              //         //   color: Colors.indigo,
-              //         // ),
-              //       ),
-              //       dotDecoration: DotDecoration(
-              //         width: 24,
-              //         height: 12,
-              //         color: Colors.grey,
-              //         // dotBorder: DotBorder(
-              //         //   padding: 2,
-              //         //   width: 2,
-              //         //   color: Colors.grey,
-              //         // ),
-              //         // borderRadius: BorderRadius.only(
-              //         //     topLeft: Radius.circular(2),
-              //         //     topRight: Radius.circular(16),
-              //         //     bottomLeft: Radius.circular(16),
-              //         //     bottomRight: Radius.circular(2)),
-              //         borderRadius: BorderRadius.circular(16),
-              //         verticalOffset: 0,
-              //       ),
-              //       spacing: 6.0,
-              //       // activeColorOverride: (i) => colors[i],
-              //       inActiveColorOverride: (i) => colors[i],
-              //     ),
-              //   ),
-              // ),
-              // const SizedBox(height: 32.0),
-            ],
-          ),
+            ),
+            SizedBox(
+              height: 32.h,
+            ),
+            Image.asset(
+              AppAssets.appLogo,
+              height: 126.h,
+              width: 126.w,
+            ),
+            SizedBox(
+              height: 66.h,
+            ),
+            Text(
+              first
+                  ? '!!نصنع الأثاث بعيونك أنت'
+                  : '!!بيت جديد!! أثاث جديد\nاكتشف تشكيلتنا المميزة من الأثاث و جدد منزلك بلمسة سحرية.',
+              style: Theme.of(context).textTheme.displayLarge,
+            ),
+            SizedBox(
+              height: 66.h,
+            ),
+            SmoothPageIndicator(
+              controller: controller,
+              count: pages.length,
+              effect: ExpandingDotsEffect(
+                activeDotColor: AppColors.brown,
+                dotColor: AppColors.grey,
+                dotHeight: 6.h,
+                dotWidth: 6.w,
+                spacing: 3,
+              ),
+            ),
+          ],
         ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            'التالي',
+            style: Theme.of(context).textTheme.displayLarge,
+          ),
+          Icon(
+            Icons.play_arrow,
+            color: AppColors.brown,
+          )
+        ],
       ),
     );
   }
 }
-
-final colors = const [
-  Colors.red,
-  Colors.green,
-  Colors.greenAccent,
-  Colors.amberAccent,
-  Colors.blue,
-  Colors.amber,
-];

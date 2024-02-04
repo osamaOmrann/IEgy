4117723ff631f0c2ed3bc0faaf0b11ff4b615b3e
AmaalOthman <engamaal180@gmail.com>
2024-02-04 22:38:32 +0200

    + test cartScreen

--- lib/features/cart/presentation/screens/cart_screen.dart
+++ lib/features/cart/presentation/screens/cart_screen.dart
@@ -7,7 +7,7 @@ class CartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Cart'),
+        title: Text('Cart Screen'),
       ),
     );
   }
